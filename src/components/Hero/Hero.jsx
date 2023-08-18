1d393b5754840b8fb1ef5e22ef206d7d1800281c
Aksh <toakshh@gmail.com>
2023-08-19 04:29:14 +0530

    lazy loading image added in hero seciton

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
                 <h1 className="md:text-7xl font-medium text-3xl">Master your life by mastering emotions</h1>
                 <div className="flex flex-row ">
                     <a href="#"><img src={Download} alt="download button" /></a>
-                    <img src={star} alt="" className="ml-6 w-32 h-12 max-w-full" />
+                    <img src={star} alt="5 start rating" className="ml-6 w-32 h-12 max-w-full" loading="lazy"/>
                 </div>
             </motion.div>
             <motion.div
@@ -31,7 +31,7 @@ const Hero = () => {
                 transition={{ duration: .9, easings:["easeInOut"] }}
                 className="md:w-1/2 flex justify-center items-center w-100 overflow-hidden"
             >
-                <img src={phone} alt="mobile" className="max-h-[85%] hover:scale-110 p-14 ease-in-out duration-500 hover:rotate-12  border-4 border-dashed border-white rounded-full" />
+                <img src={phone} alt="mobile" className="max-h-[85%] hover:scale-110 p-14 ease-in-out duration-500 hover:rotate-12  border-4 border-dashed border-white rounded-full" loading="lazy" />
             </motion.div>
         </div>
     )
