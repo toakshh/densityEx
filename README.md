fd0b33ff1046745f7d26f753823bc5e43eda8ea2
Aksh <toakshh@gmail.com>
2023-08-19 03:12:52 +0530

    readme.md updated

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
+# Ahead app clone
+
+A dynamic and animated web application built with React, Vite, and Framer Motion inspired by ahead app.
+
+![Project Demo](https://density-ahead-akshat.netlify.app) <!-- Replace with a link to your project demo GIF or screenshot -->
+
+
+## Project Overview
+
+This project is a showcase of how to create interactive and animated user interfaces using React, Vite, and Framer Motion. It demonstrates various animation techniques and best practices for creating smooth and visually appealing user experiences.
+
+## Features
+
+- List key features or functionalities of your project here.
+- For example:
+  - Interactive navigation with smooth scrolling animations
+  - Animated page transitions for a seamless browsing experience
+  - Animated components using Framer Motion's animation library
+  - ...and many more
+
+## Installation
+
+1. Clone the repository:
+
+   git clone https://github.com/toakshh/densityEx
+
+
 # React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,69 +1,80 @@
 import angryFace from "../../assets/svg/angryCard.svg"
 import CardItems from "./CardItems"
+import { motion } from "framer-motion"
+import { useRef } from "react"
 
 const Cards = () => {
-    const cardData= [{
+    const imageRef = useRef(null);
+    
+    const cardData = [{
         id: "rstieh-inrts",
-        emoji: "\u{1F603}" ,
-        title: "Lorem ipsum dolor sit" ,
+        emoji: "\u{1F603}",
+        title: "Lorem ipsum dolor sit",
         content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi perferendis aliquid quam sit similique a ad! Corrupti repellat in dolores. Nam libero ex voluptatem itaque."
     },
     {
         id: "rstieh-srtts",
-        emoji: "🥴" ,
-        title: "Lorem ipsum dolor sit" ,
+        emoji: "🥴",
+        title: "Lorem ipsum dolor sit",
         content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi perferendis aliquid quam sit similique a ad! Corrupti repellat in dolores. Nam libero ex voluptatem itaque."
     },
     {
         id: "std-srtts",
-        emoji: "🤧" ,
-        title: "Lorem ipsum dolor sit" ,
+        emoji: "🤧",
+        title: "Lorem ipsum dolor sit",
         content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi perferendis aliquid quam sit similique a ad! Corrupti repellat in dolores. Nam libero ex voluptatem itaque."
     },
     {
         id: "arts-srtts",
-        emoji: "🤕" ,
-        title: "Lorem ipsum dolor sit" ,
+        emoji: "🤕",
+        title: "Lorem ipsum dolor sit",
         content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi perferendis aliquid quam sit similique a ad! Corrupti repellat in dolores. Nam libero ex voluptatem itaque."
     },
     {
         id: "arts-hdt",
-        emoji: "😵‍💫" ,
-        title: "Lorem ipsum dolor sit" ,
+        emoji: "😵‍💫",
+        title: "Lorem ipsum dolor sit",
         content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi perferendis aliquid quam sit similique a ad! Corrupti repellat in dolores. Nam libero ex voluptatem itaque."
     },
     {
         id: "arts-fpd",
-        emoji: "🫣" ,
-        title: "Lorem ipsum dolor sit" ,
+        emoji: "🫣",
+        title: "Lorem ipsum dolor sit",
         content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi perferendis aliquid quam sit similique a ad! Corrupti repellat in dolores. Nam libero ex voluptatem itaque."
     },
     {
         id: "arts-nhdtc",
-        emoji: "😇" ,
-        title: "Lorem ipsum dolor sit" ,
+        emoji: "😇",
+        title: "Lorem ipsum dolor sit",
         content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi perferendis aliquid quam sit similique a ad! Corrupti repellat in dolores. Nam libero ex voluptatem itaque."
     },
     {
         id: "aarstd",
-        emoji: "🤮" ,
-        title: "Lorem ipsum dolor sit" ,
+        emoji: "🤮",
+        title: "Lorem ipsum dolor sit",
         content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi perferendis aliquid quam sit similique a ad! Corrupti repellat in dolores. Nam libero ex voluptatem itaque."
     },
     {
         id: "arrsthtbdtcx",
-        emoji: "🥹" ,
-        title: "Lorem ipsum dolor sit" ,
+        emoji: "🥹",
+        title: "Lorem ipsum dolor sit",
         content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi perferendis aliquid quam sit similique a ad! Corrupti repellat in dolores. Nam libero ex voluptatem itaque."
     }]
     return (
         <div className="mx-6" >
-            <h1 className="font-semibold md:text-5xl text-2xl inline-flex">Does this sound familiar... <span><img src={angryFace} alt="angryFace" className="w-20 h-20 hover:rotate-45 duration-200" /></span></h1>
+            <h1
+                className="font-semibold md:text-5xl text-2xl inline-flex"
+                ref={imageRef}
+            >   Does this sound familiar...
+                <span>
+                    <motion.img drag dragConstraints={imageRef} src={angryFace} alt="angryFace" className="w-20 h-20 hover:rotate-45 duration-200" /></span>
+            </h1>
             <div className="flex overflow-x-scroll gap-14 my-5 p-10 overflow-visible ">
                 {
-                    cardData.map(item=>{
-                        return <div className="h-fit" key={item.id}>
-                            <CardItems item={item}/>
+                    cardData.map(item => {
+                        return <div
+                            className="h-fit" key={item.id}>
+                            <CardItems item={item} />
                         </div>
                     })
                 }
