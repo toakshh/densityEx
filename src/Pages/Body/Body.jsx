9b6ea075a5fc3e50f29b7647ad0f798d65773af5
Aksh <toakshh@gmail.com>
2023-08-18 10:35:02 +0530

    best eq section implemented

--- src/Pages/Body/Body.jsx
+++ src/Pages/Body/Body.jsx
@@ -5,15 +5,27 @@ import Hero from "../../components/Hero/Hero"
 import Meet from "../../components/Meet/Meet"
 import Timeline from "../../components/Timeline/Timeline"
 
+const eqBeatsIq= {
+    heading:"EQ beats IQ",
+    card1: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi labore possimus cum perspiciatis a sapiente voluptatem itaque, beatae totam blanditiis.",
+    card2: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus recusandae corrupti nisi adipisci voluptas accusantium ab placeat ducimus beatae officiis.",
+}
+const bestIq= {
+    heading:"Be the best you with EQ",
+    card1: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi labore possimus cum perspiciatis a sapiente voluptatem itaque, beatae totam blanditiis.",
+    card2: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus recusandae corrupti nisi adipisci voluptas accusantium ab placeat ducimus beatae officiis.",
+}
+
 
 const Body = () => {
     return (
         <main className="px-8">
             <Hero />
-            <Eqbeats />
+            <Eqbeats content={eqBeatsIq}/>
             <Cards />
             <Meet />
             <Timeline />
+            <Eqbeats content={bestIq}/>
         </main>
     )
 }
