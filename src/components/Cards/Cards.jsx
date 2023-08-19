39bbe5c18675956cb14d3581c5294b466509de33
Aksh <toakshh@gmail.com>
2023-08-19 06:21:29 +0530

    cards section animation added

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,11 +1,23 @@
 import angryFace from "../../assets/svg/angryCard.svg"
 import CardItems from "./CardItems"
-import { motion } from "framer-motion"
-import { useRef } from "react"
+import { motion,useAnimation,useInView } from "framer-motion"
+import { useEffect,useRef } from "react"
 
 const Cards = () => {
     const imageRef = useRef(null);
-    
+    const inView = useInView(imageRef, {once: false})
+    const headingContent= useAnimation();
+    const cardsContent= useAnimation();
+    useEffect(()=>{
+        if(inView){
+            headingContent.start({y:0, opacity:1})
+            cardsContent.start({x:-200})
+        }
+        if(!inView){
+            cardsContent.start({x:0})
+        }
+    },[inView])
+
     const cardData = [{
         id: "rstieh-inrts",
         emoji: "\u{1F603}",
@@ -61,21 +73,26 @@ const Cards = () => {
         content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi perferendis aliquid quam sit similique a ad! Corrupti repellat in dolores. Nam libero ex voluptatem itaque."
     }]
     return (
-        <div className="mx-6" >
-            <h1
+        <div ref={imageRef} className="mx-6" >
+            <motion.h1
+                initial={{ y: 100, opacity:.5 }}
+                animate={headingContent}
+                transition={{ duration: .5 }}
                 className="font-semibold md:text-5xl text-2xl inline-flex"
-                ref={imageRef}
             >   Does this sound familiar...
                 <span>
-                    <motion.img drag dragConstraints={imageRef} src={angryFace} alt="angryFace" className="w-20 h-20 hover:rotate-45 duration-200" /></span>
-            </h1>
-            <div className="flex overflow-x-scroll gap-14 my-5 p-10 overflow-visible ">
+                    <motion.img drag dragConstraints={imageRef} src={angryFace} alt="angryFace" className="w-36 h-36 hover:rotate-45 duration-200 z-50" /></span>
+            </motion.h1>
+            <div className="flex overflow-x-scroll gap-14 my-5 p-10 overflow-visible " >
                 {
                     cardData.map(item => {
-                        return <div
-                            className="h-fit" key={item.id}>
+                        return <motion.div
+                            initial={{x:0}}
+                            animate={cardsContent}
+                            transition={{ duration: 1, easings:["easeInOut"] }}
+                            className="h-fit z-40" key={item.id}>
                             <CardItems item={item} />
-                        </div>
+                        </motion.div>
                     })
                 }
             </div>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,20 +1,37 @@
 import Download from "../../assets/svg/Download_btn.svg"
 import star from "../../assets/svg/5star.svg"
 import phone from "../../assets/svg/mobile.svg"
-import { motion } from "framer-motion"
+import { motion,useAnimation,useInView } from "framer-motion"
+import { useEffect,useRef } from "react"
 
 const Hero = () => {
+    const ref=useRef(null);
+    const inView = useInView(ref, {once: false})
+    const rightContent= useAnimation();
+    const leftContent= useAnimation();
+    useEffect(()=>{
+        // console.log(inView)
+        if(inView){
+            leftContent.start({ x: 0 })
+            rightContent.start({x:0})
+        }
+        if(!inView){
+            leftContent.start({x:-200})
+            rightContent.start({x:200})
+        }
+    },[inView])
     return (
-        <div className="md:mx-6 mx-2 rounded-3xl md:h-screen flex flex-col-reverse md:flex-row" style={{ backgroundColor: '#EEEBFE' }}>
+        <div ref={ref} className="md:mx-6 mx-2 rounded-3xl md:h-screen flex flex-col-reverse md:flex-row" style={{ backgroundColor: '#EEEBFE' }}>
             <motion.div
+                
                 initial={{ x: -200 }}
-                animate={{ x: 0 }}
+                animate={leftContent}
                 transition={{ duration: .9, easings:["easeInOut"] }}
                 className="md:w-1/2 flex flex-col justify-center items-start md:ml-10 ml-5 gap-7 w-100"
             >
                 <motion.h4 
                 initial={{ x: -200 }}
-                animate={{ x: 0 }}
+                animate={leftContent}
                 transition={{ duration: 1.5 }}
                     className="md:text-3xl font-medium pl-2 "
                 >Ahead app
@@ -27,11 +44,12 @@ const Hero = () => {
             </motion.div>
             <motion.div
                 initial={{ x: 200, }}
-                animate={{ x: 0, }}
+                animate={rightContent}
                 transition={{ duration: .9, easings:["easeInOut"] }}
                 className="md:w-1/2 flex justify-center items-center w-100 overflow-hidden"
             >
-                <img src={phone} alt="mobile" className="max-h-[85%] hover:scale-110 p-14 ease-in-out duration-500 hover:rotate-12  border-4 border-dashed border-white rounded-full"  />
+                <img 
+                    src={phone} alt="mobile" className="max-h-[85%] hover:scale-110 p-14 ease-in-out duration-500 hover:rotate-12  border-4 border-dashed border-white rounded-full"  />
             </motion.div>
         </div>
     )
